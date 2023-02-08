de53f9e33d4e642f034646867c0721848f9ee60d
callmhejerry <chinedujeremiah2002@gmail.com>
2023-02-08 13:46:04 +0100

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -15,14 +15,28 @@ class BaseModel():
     A BaseModel class that defines all common methods
     and attributes for other classes
     """
-
-    def __init__(self):
+    
+    def __init__(self, *args, **kwargs):
         """
-        creates a new instance of the BaseModel class
+        *args and **kwargs arguments for the BaseModel
+        if kwargs is not empty:
+        each key of this dictionary is an attribute name
+        (Note __class__ from kwargs is the only one
+        that should not be added as an attribute
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"],
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
         '''
@@ -47,25 +61,3 @@ class BaseModel():
         dct["created_at"] = self.created_at.isoformat()
         dct["updated_at"] = self.updated_at.isoformat()
         return dct
-
-    def __init__(self, *args, **kwargs):
-        """
-        *args and **kwargs arguments for the BaseModel
-        if kwargs is not empty:
-        each key of this dictionary is an attribute name
-        (Note __class__ from kwargs is the only one
-        that should not be added as an attribute
-        """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if "created_at" in kwargs:
-                self.created_at = datetime.strptime(kwargs["created_at"],
-                                                    "%Y-%m-%dT%H:%M:%S.%f")
-            if "updated_at" in kwargs:
-                self.updated_at = datetime.strptime(kwargs["updated_at"],
-                                                    "%Y-%m-%dT%H:%M:%S.%f")
