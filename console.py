6d7f16a6d7ca50a66fda4c0b5cf0fd13e3135296
callmhejerry <chinedujeremiah2002@gmail.com>
2023-02-12 01:17:06 +0100

    added the __init__ method for the tests folder

--- console.py
+++ console.py
@@ -5,10 +5,30 @@ The console command line intepreter
 
 import json
 import cmd
+import re
+from shlex import split
 from models.base_model import BaseModel
 from models.engine.file_storage import *
 from models import storage
 import sys
+
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBCommand(cmd.Cmd):
