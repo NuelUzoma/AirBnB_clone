f110e5e8529e162e9d829793b92c0c577fdf0594
callmhejerry <chinedujeremiah2002@gmail.com>
2023-02-13 15:21:03 +0100

    hot fix

--- console.py
+++ console.py
@@ -13,24 +13,6 @@ from models import storage
 import sys
 
 
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
-
-
 class HBNBCommand(cmd.Cmd):
     """Simple command processor example"""
     prompt = '(hbnb) '
@@ -102,7 +84,6 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         """The End of File for the console"""
-        print()
         return True
 
     def emptyline(self):
